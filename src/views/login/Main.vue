cd0dc34203e210f4492a362f54547ec0de53e1ed
Sinshevan <93983564+Sinshevan@users.noreply.github.com>
2021-11-18 18:41:29 +0700

    belajar router view

---
+++ src/views/login/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="text-center">
+        <h1>LOGIN</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/register/Main.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">Register</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Register'
+}
+</script>
