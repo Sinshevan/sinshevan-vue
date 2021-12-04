688e9bc71b937ac5e0812b6fcd99cc1513eab72d
Sinshevan <93983564+Sinshevan@users.noreply.github.com>
2021-12-04 19:05:54 +0700

    Latihan

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,37 @@ const routes = [
         path: '/register',
         name: 'register',
         component: () => import('@/views/register/Main.vue')
-    }
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/Main')
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/Main')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('@/views/about/Main')
+    },
+    {
+        path: '/users',
+        name: 'users',
+        component: () => import('@/views/users/Main')
+    },
+    {
+        path: '/users/:id/:username?',
+        name: 'detailuser',
+        component: () => import('@/views/users/DetailUser')
+    },
+    {
+        path: '/create-user',
+        name: 'createuser',
+        component: () => import('@/views/users/FormCreate')
+    },
 ];
 
 const router = createRouter({
